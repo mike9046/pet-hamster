a39ab4f145dcf599b1259f257b724679fc0b7096
mike9046 <87992264+mike9046@users.noreply.github.com>
2021-08-07 08:27:23 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    soundExpression.sad.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
+})
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    soundExpression.giggle.playUntilDone()
+})
